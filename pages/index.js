58e368ab038b9251a38788e45a72d0de802f4e23
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-24 19:10:47 +0200

    data showing in db

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import prisma from "@/lib/prisma"
-import { fromJSON } from "postcss"
 import { getJobs } from "@/lib/data"
 import Jobs from "./components/Jobs"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function Index({jobs}) {
+  const {data: session, status} = useSession()
+  const router = useRouter()
+  
+  if(session && !session.user.name){
+    router.push('/setup')
+  }
   return (
     <div className='mt-10'>
       <div className='text-center p-4 m-4'>
         <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
       </div>
+      {!session && (
+        <a
+        className="flex justify-center w-24 px-8 py-2 mx-auto font-bold text-white bg-black border border-black rounded-full"
+        href="/api/auth/signin"
+        >login</a>
+      )}
        <Jobs jobs={jobs}/>
         
     
