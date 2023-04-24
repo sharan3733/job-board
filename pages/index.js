dfccc8c6c0c652a7bce45002471523deb39d808c
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-24 17:07:36 +0200

    create a page 'home' of a single company, where list all the jobs they posted

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
+import prisma from "@/lib/prisma"
+import { fromJSON } from "postcss"
+import { getJobs } from "@/lib/data"
+import Jobs from "./components/Jobs"
 
-export default function Home() {
-
+export default function Index({jobs}) {
   return (
-   
-    <div>
-      <h1>Home Page</h1>
-      <a href='/api/auth/signin'>login</a>
-    </div>
- 
+    <div className='mt-10'>
+      <div className='text-center p-4 m-4'>
+        <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
+      </div>
+       <Jobs jobs={jobs}/>
+        
+    
+    </div> 
   )
- 
+}
+
+export async function getServerSideProps(context) {
+ /*const jobs = await prisma.job.findMany({
+    where: {
+      published: true
+    },
+    orderBy: [
+      {
+        id: 'desc',
+      },
+    ],
+    include: {
+      author:true,
+    },
+  })*/
+
+  let jobs = await getJobs(prisma)
+  jobs = JSON.parse(JSON.stringify(jobs))
+  return {
+    props: {
+      jobs,
+    },
+  }
 }
