8ae15f16a0756a68a92b14e64f065399eabb9cc3
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-25 11:53:42 +0200

    different options currently logged-in user

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,96 @@
 import prisma from "@/lib/prisma"
-import { getJobs } from "@/lib/data"
+import { getJobs, getUser } from "@/lib/data"
 import Jobs from "./components/Jobs"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
-
-export default function Index({jobs}) {
-  const {data: session, status} = useSession()
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]"
+import Link from "next/link"
+export default function Index({ jobs, user }) {
+  const { data: session, status } = useSession()
   const router = useRouter()
-  
-  if(session && !session.user.name){
+
+  if (session && !session.user.name) {
     router.push('/setup')
   }
   return (
+
     <div className='mt-10'>
       <div className='text-center p-4 m-4'>
         <h2 className='mb-10 text-4xl font-bold'>Find a job!</h2>
       </div>
       {!session && (
-        <a
-        className="flex justify-center w-24 px-8 py-2 mx-auto font-bold text-white bg-black border border-black rounded-full"
-        href="/api/auth/signin"
-        >login</a>
+        // eslint-disable-next-line @next/next/no-html-link-for-pages
+        <a className='flex justify-center w-24 px-8 py-2 mx-auto font-bold text-white border bg-black border-black rounded-full'
+          href='/api/auth/signin'>login</a>
+      )
+      }
+
+      {session && (
+        <div class='mx-auto text-center'>
+          <p className='mb-10 text-2xl font-normal'>
+            Welcome, {user.name}
+            {user.company && (
+              <span className='bg-black text-white uppercase text-sm p-2'>
+                Company
+              </span>
+            )}
+          </p>
+          {user.company ? (
+            <>
+            <Link href={`/new`}>
+              <button
+                className='border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                click here to post a new job
+              </button>
+              </Link>
+              <button
+                className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                see all the jobs you posted
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className='ml-5 border px-8 py-2 mt-5 font-bold rounded-full bg-black text-white border-black '
+              >
+                see all the jobs you applied to
+              </button>
+            </>
+          )}
+        </div>
       )}
-       <Jobs jobs={jobs}/>
-        
-    
-    </div> 
+
+      <Jobs jobs={jobs} />
+
+
+    </div>
+
   )
 }
 
 export async function getServerSideProps(context) {
- /*const jobs = await prisma.job.findMany({
-    where: {
-      published: true
-    },
-    orderBy: [
-      {
-        id: 'desc',
-      },
-    ],
-    include: {
-      author:true,
-    },
-  })*/
+  const session = await getServerSession(context.req, context.res, authOptions)
+
 
   let jobs = await getJobs(prisma)
   jobs = JSON.parse(JSON.stringify(jobs))
+
+  if (!session) {
+    return {
+      props: { jobs },
+    }
+  }
+
+  let user = await getUser(session.user.id, prisma)
+  user = JSON.parse(JSON.stringify(user))
+
   return {
     props: {
       jobs,
+      user,
     },
   }
 }
